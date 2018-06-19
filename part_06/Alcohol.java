92c5d3e7c6c338c8bc06259ef1b24cfad2fde5d7
Ashley Almeida <ashleyalmeida@Ashleys-MacBook-Pro.local>
2018-06-18 17:16:42 -0700

    Alcohol package

---
+++ part_06/Alcohol.java
@@ -0,0 +1,46 @@
+package part_06;
+
+
+// controller
+class AlcoholControl {
+    public static void main(String[] args) {
+
+
+    }
+}
+
+// Hard liquor
+class HardLiquor {
+    int alcPercentage;
+    int
+
+}
+
+// extends Hard liquor
+class Tequila extends HardLiquor {
+
+
+}
+
+
+// extends Tequila
+class PatronTequila extends Tequila
+
+
+// entends Hard Liquor
+class Whiskey extends HardLiquor {
+
+
+}
+
+// extends Whiskey > Hard Liquor
+
+class BushmillsWhiskey extends Whiskey {
+
+
+}
+
+
+class JamesonWhiskey extends Whiskey {
+
+}
